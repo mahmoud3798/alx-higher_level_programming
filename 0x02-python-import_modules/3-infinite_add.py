4b1cb7479b955874b88fad65a301254229a339fa
mahmoud <mahmoud.m3798@gmail.com>
2023-06-08 15:10:37 -0700

    Task 3

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import hidden_4
+    import sys
 
-    for name in dir(hidden_4):
-        if name[:2] != "__":
-            print(name)
+    res = 0
+    args = sys.argv[1:]
+    num_args = len(args)
+    for i in range(1, num_args):
+        res += int(sys.argv[i])
+    print(res)
