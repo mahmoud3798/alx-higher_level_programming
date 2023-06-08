c85f35f28de52a33ade73df28b214a3de9d53ff6
mahmoud <mahmoud.m3798@gmail.com>
2023-06-08 15:11:54 -0700

    Task 3

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -3,7 +3,7 @@ if __name__ == "__main__":
     import sys
 
     res = 0
-    args = sys.argv[1:]
+    args = sys.argv[:]
     num_args = len(args)
     for i in range(1, num_args):
         res += int(sys.argv[i])
