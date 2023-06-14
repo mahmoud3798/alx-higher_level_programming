656558fdeadc79097a83befc1c9f2d79f4426378
mahmoud <mahmoud.m3798@gmail.com>
2023-06-14 05:21:28 -0700

    Task 12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -3,8 +3,7 @@ def roman_to_int(roman_string):
     if not isinstance(roman_string, str):
         return 0
     
-    roman_dict = {'I': 1, 'IV': 4, 'V': 5, 'IX': 9, 'X': 10,
-	'XL': 40, 'L': 50, 'XC': 90, 'C': 100, 'CD': 400, 'D': 500, 'CM': 900, 'M': 1000}
+    roman_dict = {'I': 1, 'IV': 4, 'V': 5, 'IX': 9, 'X': 10, 'XL': 40, 'L': 50, 'XC': 90, 'C': 100, 'CD': 400, 'D': 500, 'CM': 900, 'M': 1000}
     result = 0
     i = 0
     
