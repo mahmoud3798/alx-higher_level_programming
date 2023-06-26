51e5a3735602ce782e9f02efd572b57c549ffbfd
mahmoud <mahmoud.m3798@gmail.com>
2023-06-26 05:09:42 -0700

    Update

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -6,6 +6,6 @@ def safe_print_list(my_list=[], x=0):
             print("{}".format(my_list[i]), end="")
             count += 1
         except (TypeError, ValueError):
-            continue
+            break
     print()
     return count
