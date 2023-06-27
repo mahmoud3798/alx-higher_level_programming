39fbfce3bb2be15fbf2590eeb825b85cbb0630b7
mahmoud <mahmoud.m3798@gmail.com>
2023-06-27 05:17:31 -0700

    Task 3

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Square with size"""
+
+
+class Square:
+    """square"""
+
+    def __init__(self, size=0):
+        """Intialize size"""
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        """Public instance method that returns the current square area"""
+        return self.__size ** 2
